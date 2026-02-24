8ba2789d597603d27859812cb403f7b76ef9e2fd
Guilherme Gomes <guilhermesg@hotmail.com>
2026-02-24 17:19:15 -0300

    Initial frontend structure

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard/Dashboard.tsx'
+import Login from './pages/Login.tsx'
+
+function Router() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
